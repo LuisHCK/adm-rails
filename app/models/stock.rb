4a303075928439213bb3af97bcf6a78d5feea024
LuisHCK <luisjcenteno@live.com>
2017-03-21 23:08:03 -0600

    Stuff for stock -Validates if lot code are unique -Return price of product in stock

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,13 @@
 class Stock < ApplicationRecord
     belongs_to :product
+    
+    validates :lot, uniqueness: true
 
     def stock_name
         return self.product.name
+    end
+
+    def stock_price
+        return self.product.sale_price
     end
 end
